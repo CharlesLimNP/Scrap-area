60f75083dd2fe021616eeb1fe585e3d441923334
CharlesLimNP <Charles_Lim@np.edu.sg>
2022-02-25 15:26:51 +0800

    amended docstrings

--- memberB_module.py
+++ memberB_module.py
@@ -1,6 +1,6 @@
 def evenOrOdd(number):
     """
-    Takes an integer and determine if it is an odd or even number
+    Takes an integer and determine if it is an odd or even number. Returns string "odd" or "even"
     """ 
     if (number % 2) == 0:
         return "even"
