8c9c1326764d53a6f19759445fd7d391f9708161
realbigbeer <realbigbeer@gmail.com>
2022-02-25 15:18:57 +0800

    implemented everOrOdd(number)

--- memberB_module.py
+++ memberB_module.py
@@ -1,1 +1,8 @@
-# empty module
+def evenOrOdd(number):
+    """
+    Takes an integer and determine if it is an odd or even number
+    """ 
+    if (number % 2) == 0:
+        return "even"
+    else:
+        return "odd"
