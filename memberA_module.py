eba4d07d4e59543d2c30fe23304202b11332eb66
realbigbear88 <realbigbear@gmail.com>
2022-02-25 15:15:58 +0800

    implemented function add(*numbers)

--- memberA_module.py
+++ memberA_module.py
@@ -1,1 +1,8 @@
-# empty module
+def add(*numbers):
+    """
+    Compute the sum of all numbers supplied that are separated by a comma
+    """
+    number = 0
+    for num in numbers:
+        number += num
+    return number
